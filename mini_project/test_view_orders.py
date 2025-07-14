a741e8de41ccb2901d777a1d3507023ba1c02edd
Zoya Khan <zoyak.genstudents.org>
2025-07-14 18:56:12 +0100

    created a test for view orders by status

--- mini_project/test_view_orders.py
+++ mini_project/test_view_orders.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 
 # happy case 
 
-def test_happy_view_orders_status_with_results():
+def test_happy_view_orders_by_status_with_results():
     # arrange some test data
     status_id = 1
 
@@ -16,3 +16,40 @@ def test_happy_view_orders_status_with_results():
        
     assert result is None
    
+@patch("builtins.print")
+def test_happy_view_order_by_status(mock_print):
+    # Arrange
+    status_id = 1 # exisitng data in sql table 
+
+    #act
+    view_orders_by_status(status_id)
+
+    # assert
+    mock_print.assert_any_call("order_id: 1, name: Sam Smith, status: preparing, courier: deliveroo")
+
+# unhappy case
+
+@patch("builtins.print")
+def test_unhappy_view_order_by_inavlid_status(mock_print):
+    # arrange
+    status_id = 99 # status id num does not exist 
+
+    # act
+    view_orders_by_status(status_id)
+
+    # assert
+    mock_print.asssert_any_call("No orders with that status.")
+
+# edge case 
+@patch("builtins.print")
+def test_edge_view_order_by_empty_status(mock_print):
+    # arrange
+    status_id = 4 # valid status but linked to no order 
+
+    # act
+    view_orders_by_status(status_id)
+
+    # assert
+    mock_print.assert_any_call("No orders found with that status.")
+     
+    
